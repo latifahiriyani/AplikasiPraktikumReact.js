6776080fb0927d0bdb4852cb58f5f0685dfdc47d
latifahiriyani <latifahiriyani98@gmail.com>
2020-06-08 09:28:26 +0700

    sampai video 26 reactstrap layout

---
+++ Documents/codee/aplikasipertama/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Parent from './Component/Class/Parent';
+import BootstrapComp from './Component/Class/BootstrapComp';
+
+//import logo from './logo.svg';
+//import Home from './Component/Fungsional/Home';
+//import Beranda from './Component/Class/Beranda';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <BootstrapComp/>
+
+    </div>
+  );
+}
+
+export default App;
