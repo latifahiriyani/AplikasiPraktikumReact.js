9249bdaddec2e0b64137b3ac56ba91bd6c87e532
latifahiriyani <latifahiriyani98@gmail.com>
2020-06-13 08:13:45 +0700

    Jumbotron

--- Documents/codee/aplikasipertama/src/App.js
+++ Documents/codee/aplikasipertama/src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 //import Parent from './Component/Class/Parent';
 //import BootstrapComp from './Component/Class/BootstrapComp';
 import NavbarComp from './Component/Fungsional/NavbarComp';
+import HomePage from './Component/Fungsional/HomePage';
+import About from './Component/Fungsional/AboutComp';
 
 //import logo from './logo.svg';
 //import Home from './Component/Fungsional/Home';
 //import Beranda from './Component/Class/Beranda';
 //import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     <NavbarComp/>
+    <BrowserRouter>
+      <NavbarComp />
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={About} />
+      </switch>
+    </BrowserRouter>
 
-    </div>
+
+
+
   );
 }
 
