57a68727a07c6fe33b6de394ced69ace575d8dc9
latifahiriyani <latifahiriyani98@gmail.com>
2020-06-21 10:14:06 +0700

    ListComp Data Mahasiswa

--- Documents/codee/aplikasipertama/src/App.js
+++ Documents/codee/aplikasipertama/src/App.js
@@ -5,8 +5,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import NavbarComp from './Component/Fungsional/NavbarComp';
 import HomePage from './Component/Fungsional/HomePage';
 import About from './Component/Fungsional/AboutComp';
-import DetailComp from './Component/Fungsional/DetailComp';
-
+//import DetailComp from './Component/Fungsional/DetailComp';
+import ListComp from './Component/Class/ListComp';
 
 //import logo from './logo.svg';
 //import Home from './Component/Fungsional/Home';
@@ -20,7 +20,9 @@ const App = () => {
       <switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/about" component={About} />
-        <Route exact path="/detail/:id" component={DetailComp} />
+        <Route exact path="/mahasiswa" component={ListComp} />
+
+       {/*<Route exact path="/detail/:id" component={DetailComp} />*/}
       </switch>
     </BrowserRouter>
 
