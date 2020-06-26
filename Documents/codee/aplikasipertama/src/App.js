311321e1354ad36aeade6b228053b2f3d0d1e429
latifahiriyani <latifahiriyani98@gmail.com>
2020-06-26 07:23:41 +0700

    Berhasil Edit Data Mahasiswa Video 32

--- Documents/codee/aplikasipertama/src/App.js
+++ Documents/codee/aplikasipertama/src/App.js
@@ -8,6 +8,7 @@ import About from './Component/Fungsional/AboutComp';
 //import DetailComp from './Component/Fungsional/DetailComp';
 import ListComp from './Component/Class/ListComp';
 import TambahComp from './Component/Class/TambahComp';
+import EditComp from './Component/Class/EditComp';
 
 
 //import logo from './logo.svg';
@@ -23,7 +24,8 @@ const App = () => {
         <Route exact path="/" component={HomePage} />
         <Route exact path="/about" component={About} />
         <Route exact path="/mahasiswa" component={ListComp} />
-         <Route exact path="/mahasiswa/tambah" component={TambahComp} />
+        <Route exact path="/mahasiswa/tambah" component={TambahComp} />
+        <Route exact path="/mahasiswa/edit" component={EditComp} />
 
        {/*<Route exact path="/detail/:id" component={DetailComp} />*/}
       </switch>
--- Documents/codee/aplikasipertama/src/Component/Class/ListComp.jsx
+++ Documents/codee/aplikasipertama/src/Component/Class/ListComp.jsx
@@ -1,59 +1,82 @@
-import React, {PureComponent} from 'react'
+import React, {PureComponent } from 'react'
 import axios from 'axios'
 import qs from 'querystring'
 import {Table, Button, Container, NavLink, Alert} from 'reactstrap'
+import {Link} from 'react-router-dom'
 
 const api = 'http://localhost:3001'
 
 class ListComp extends PureComponent {
     constructor(props) {
-        super(props)
+        super (props)
         this.state = {
             mahasiswa: [],
             response: '',
-            diaplay: 'none'
-
+            display: 'none'
         }
     }
+
     componentDidMount(){
         axios.get(api+'/tampil').then(res=>{
             this.setState({
-                mahasiswa: res.data.values
+                mahasiswa: res.data.values 
             })
         })
     }
 
-render() {
-    return (
-        <Container>
-            <h2>Data Mahasiswa</h2>
-            <NavLink href="/mahasiswa/tambah"><Button color="success">Tambah Data</Button></NavLink>
-            <hr/>
-            <Table className="table=bordered">
-                <thead>
-                    <tr>
-                        <th>NIM</th>
-                        <th>Nama</th>
-                        <th>Jurusan</th>
-                        <th>Aksi</th>
+
+
+    render () {
+        return (
+            <Container>
+                <h2>Data Mahasiswa</h2>
+                
+                <NavLink href="/mahasiswa/tambah"><button color="success">Tambah Data</button></NavLink>
+                <hr/>
+                <Table className="table-bordered">
+                    <thead>
+                        <tr>
+                            <th>NIM</th>
+                            <th>Nama</th>
+                            <th>Jurusan</th>
+                            <th>Aksi</th>
                         </tr>
-                    
-                </thead>
-                <tbody>
-                    {this.state.mahasiswa.map(mahasiswa =>
-                    <tr key={mahasiswa.id_mahasiswa}>
-                        <td>{mahasiswa.nim}</td>
-                        <td>{mahasiswa.nama}</td>
-                        <td>{mahasiswa.jurusan}</td>
-                        <td>Edit | Hapus</td>
-                    </tr>
-                        )}
-                </tbody>
-            </Table>
-        </Container>
-    )
+                    </thead>
+                    <tbody>
+                        {this.state.mahasiswa.map(mahasiswa =>
+                            <tr key={mahasiswa.id_mahasiswa}>
+                                <td>{mahasiswa.nim}</td>
+                                <td>{mahasiswa.nama}</td>
+                                <td>{mahasiswa.jurusan}</td>
+                                <td>
+                                    <Link to= 
+                                    {
+                                        {
+                                            pathname: '/mahasiswa/edit',
+                                            state: {
+                                                id_mahasiswa: mahasiswa.id_mahasiswa,
+                                                nim: mahasiswa.nim,
+                                                nama: mahasiswa.nama,
+                                                jurusan: mahasiswa.jurusan
+                                            }
+                                        }
+                                    }>
+                                        <Button>Edit</Button>
+                                        
+                                    </Link>
+                                    
 
-}
-}
 
-export default ListComp
+                                </td>
+                            </tr>
+
+                            )}
+                    </tbody>
+
+                </Table>
+            </Container>
+        )
+    }
+} 
+
+export default ListComp;
