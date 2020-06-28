500e5b3ac2d98ec0f4b73a15366c04a35b1be891
latifahiriyani <latifahiriyani98@gmail.com>
2020-06-28 12:59:48 +0700

    Berhasil login, selesai video 34

--- Documents/codee/aplikasipertama/src/Component/Hooks/Functional/HooksComp.jsx
+++ Documents/codee/aplikasipertama/src/Component/Hooks/Functional/HooksComp.jsx
@@ -1,15 +1,27 @@
 import React, {useState} from 'react';
-
+import TambahComp from '../../Class/TambahComp';
+import TampilComp from './TampilComp';
 
 const HooksComp = () => {
-    const [jumlah, setJumlah] = useState(0)
+    const [jumlah, tambahJumlah] = useState(0)
 
-    
+    //login
+    const [dataLogin, setDataLogin] = useState({username:'firda', token: '123abcd', isLogin: true})
+
+    //cek kondisi sudah login/belum
+
+    let tampil;
+    if(dataLogin.isLogin){
+        tampil = <TampilComp
+        username = {dataLogin.username}
+        fungsi = {tambahJumlah.bind(this)}
+        jumlah = {jumlah} />
+    } else{
+        tampil = <TampilComp username="Maaf anda belum login" disabled = {true}/>
+    }
     return (
         <div>
-           <h1>{jumlah}</h1>
-           <button onClick={()=>setJumlah(jumlah+1)}>Tambah</button>
-
+           {tampil}
         </div>
     )
 }
