b33d3b1af18868206a0598b0017b856389dae555
latifahiriyani <latifahiriyani98@gmail.com>
2020-07-03 07:37:40 +0700

    Berhasil menambahkan data pada use effects

--- Documents/codee/aplikasipertama/src/Component/Hooks/Functional/HooksUseEffects.jsx
+++ Documents/codee/aplikasipertama/src/Component/Hooks/Functional/HooksUseEffects.jsx
@@ -1,20 +1,53 @@
-import React, {useEffect, useState} from 'react'
-import { Container } from 'reactstrap'
+import React, { useEffect, useState } from 'react'
+import { Container, Table, Button } from 'reactstrap'
+import axios from 'axios'
+
+const api = 'http://localhost:3001'
 
 function HooksUseEffect() {
 
-    const[namaLengkap, setNamaLengkap] = useState({nama:'Andika', marga:'Wijaya'})
+    const [mahasiswa, setMahasiswa] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log("Memanggil Use Effects")
-        setNamaLengkap({nama:'Vincent', marga:'Tan'})
-    },[])
+        axios.get(api + '/tampil').then(res => {
+            setMahasiswa(res.data.values)
+        })
+    }, [])
 
     return (
-       <Container>
-            <h3>Profil User</h3>
-            <h4>Nama : {namaLengkap.nama}</h4>
-            <h4>Marga : {namaLengkap.marga}</h4>
+        <Container>
+            <h2>Data Mahasiswa</h2>
+            <hr />
+            <Table className="table-bordered">
+                <thead>
+                    <tr>
+                        <th>NIM</th>
+                        <th>Nama</th>
+                        <th>Jurusan</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {mahasiswa.map(mahasiswa =>
+                        <tr key={mahasiswa.id_mahasiswa}>
+                            <td>{mahasiswa.nim}</td>
+                            <td>{mahasiswa.nama}</td>
+                            <td>{mahasiswa.jurusan}</td>
+                            <td>
+                                <Button>Edit</Button>
+                                <span> </span>
+                                <Button color="danger">Hapus</Button>
+
+
+
+                            </td>
+                        </tr>
+
+                    )}
+                </tbody>
+
+            </Table>
         </Container>
     )
 }
