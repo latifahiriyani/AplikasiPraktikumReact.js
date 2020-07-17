3306b10599a4470240231b80354fa57acbce9f1a
latifahiriyani <latifahiriyani98@gmail.com>
2020-07-17 07:28:50 +0700

    menambahkan card pada menu Reducer

--- Documents/codee/aplikasipertama/src/Component/Hooks/Functional/HookReducer.jsx
+++ Documents/codee/aplikasipertama/src/Component/Hooks/Functional/HookReducer.jsx
@@ -1,12 +1,18 @@
-import {Container, Row, Col} from 'react'
+import { Container, Row, Col, Card, CardImg } from 'reactstrap'
 import React from 'react'
 
 function HookReducer() {
     return (
         <Container>
+            <br/>
             <Row>
-            <Col xs="6">col 1</Col>
-            <Col xs="6">col 2</Col>
+                <Col xs="6">
+                    <Card>
+                        <CardImg top width="100%" src="http://placeimg.com/640/480/tech" alt="Card image cap" />
+
+                    </Card>
+                </Col>
+                <Col xs="6">col 2</Col>
             </Row>
         </Container>
     )
