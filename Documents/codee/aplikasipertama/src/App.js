d441dcc0ca12b238ed252573ce52e9a9c8bd8b58
latifahiriyani <latifahiriyani98@gmail.com>
2020-07-17 07:18:53 +0700

    tambah menu Reducer

--- Documents/codee/aplikasipertama/src/App.js
+++ Documents/codee/aplikasipertama/src/App.js
@@ -15,6 +15,7 @@ import HooksComp from './Component/Hooks/Functional/HooksComp';
 import HooksUseEffect from './Component/Hooks/Functional/HooksUseEffects';
 import { CartContext } from './CartContext';
 import ProductComp from './Component/Hooks/Functional/ProductComp';
+import HookReducer from './Component/Hooks/Functional/HookReducer';
 
 
 //import logo from './logo.svg';
@@ -41,6 +42,7 @@ const[value, setValue] = useState(0)
           <Route exact path="/hooks" component={HooksComp} />
           <Route exact path="/useeffects" component={HooksUseEffect} />
           <Route exact path="/produk" component={ProductComp} />
+          <Route exact path="/reducer" component= {HookReducer} />
 
           {/*<Route exact path="/detail/:id" component={DetailComp} />*/}
         </switch>
---
+++ Documents/codee/aplikasipertama/src/Component/Hooks/Functional/HookReducer.jsx
@@ -0,0 +1,15 @@
+import {Container, Row, Col} from 'react'
+import React from 'react'
+
+function HookReducer() {
+    return (
+        <Container>
+            <Row>
+            <Col xs="6">col 1</Col>
+            <Col xs="6">col 2</Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default HookReducer
